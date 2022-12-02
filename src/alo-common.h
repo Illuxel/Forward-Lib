6d87dd244941654d39e6d23146b1c39d85b28039
EEdle <83777873+EEdle506@users.noreply.github.com>
2022-12-02 03:46:42 +0200

    Renamed default lib define (aloAPI_global.h)

--- src/alo-common.h
+++ src/alo-common.h
@@ -2,8 +2,14 @@
 
 #include <QtGlobal>
 
+#include <QtCore>
+#include <QtNetwork>
+#include <QtSql>
+
 #if defined(ALO_EXPORT)
 #  define ALO_API Q_DECL_EXPORT
 #else
 #  define ALO_API Q_DECL_IMPORT
 #endif
+
+namespace alo {}
--- src/aloAPI.h
+++ src/aloAPI.h
@@ -1,8 +1,16 @@
 #pragma once
 
-#include "utils/log.h"
+#include "utils/debug/log.h"
+#include "utils/debug/logger.h"
+
 #include "utils/converters.h"
 
 #include "datatypes/Account.h"
+//#include "datatypes/Socket.h"
+
 #include "datatypes/Message.h"
 #include "datatypes/MessageForward.h"
+
+#include "datatypes/Chat.h"
+//#include "datatypes/Group.h"
+//#include "datatypes/"
