d495312c08dbe709cb86219b2a4f69b8349618da
EEdle <83777873+EEdle506@users.noreply.github.com>
2023-05-22 02:00:03 +0300

    Added Flags class

---
+++ flutils/fl/utils/Flags.hpp
@@ -0,0 +1,127 @@
+#pragma once
+
+#include <type_traits>
+
+namespace fl::utils {
+
+    template <typename Enum>
+    class EnumFlags
+    {
+        static_assert(std::is_enum_v<Enum>, "EnumFlags requires an enum type.");
+
+        using underlying_type = std::underlying_type_t<Enum>;
+
+        underlying_type enum_val_;
+
+    public:
+        constexpr EnumFlags() noexcept
+            : enum_val_{} {}
+        constexpr EnumFlags(Enum value) noexcept
+            : enum_val_{static_cast<underlying_type>(value)} {}
+
+        constexpr EnumFlags(EnumFlags const& value) noexcept
+            : enum_val_{value.enum_val_} {}
+
+        constexpr bool Test(EnumFlags other) const 
+        {
+            return (enum_val_ & static_cast<underlying_type>(other)) != 0;
+        }
+
+        constexpr bool operator!() const noexcept
+        {
+            return enum_val_ == 0;
+        }
+        constexpr bool operator==(EnumFlags other) const noexcept
+        {
+            return enum_val_ == other.enum_val_;
+        }
+        constexpr bool operator!=(EnumFlags other) const noexcept
+        {
+            return enum_val_ != other.enum_val_;
+        }
+
+        constexpr EnumFlags& operator|=(EnumFlags other) noexcept
+        {
+            enum_val_ |= other.enum_val_;
+            return *this;
+        }
+        constexpr EnumFlags& operator&=(EnumFlags other) noexcept
+        {
+            enum_val_ &= other.enum_val_;
+            return *this;
+        }
+        constexpr EnumFlags& operator^=(EnumFlags other) noexcept
+        {
+            enum_val_ ^= other.enum_val_;
+            return *this;
+        }
+        
+        constexpr EnumFlags operator|(EnumFlags other) const noexcept
+        {
+            return EnumFlags{*this} |= other;
+        }
+        constexpr EnumFlags operator&(EnumFlags other) const noexcept
+        {
+            return EnumFlags{*this} &= other;
+        }
+        constexpr EnumFlags operator^(EnumFlags other) const noexcept
+        {
+            return EnumFlags{*this} ^= other;
+        }
+        constexpr EnumFlags operator~() const noexcept
+        {
+            return EnumFlags{static_cast<Enum>(~enum_val_)};
+        }
+
+        constexpr explicit operator bool() const noexcept
+        {
+            return enum_val_ != 0;
+        }
+        constexpr explicit operator underlying_type() const noexcept
+        {
+            return enum_val_;
+        }
+    };
+}
+
+#define ENABLE_ENUM_FLAGS(Enum, FlagsName) \
+    using FlagsName = fl::utils::EnumFlags<Enum>
+
+#define DECLARE_ENUM_OPERATORS(Enum) \
+    static_assert(std::is_enum_v<Enum>, "EnumFlags requires an enum type."); \
+    \
+    inline constexpr Enum operator|(Enum lhs, Enum rhs) noexcept \
+    {                                                            \
+        return static_cast<Enum>(                                \
+            static_cast<std::underlying_type_t<Enum>>(lhs) |     \
+            static_cast<std::underlying_type_t<Enum>>(rhs));     \
+    }                                                            \
+    \
+    inline constexpr Enum operator&(Enum lhs, Enum rhs) noexcept \
+    {                                                            \
+        return static_cast<Enum>(                                \
+            static_cast<std::underlying_type_t<Enum>>(lhs) &     \
+            static_cast<std::underlying_type_t<Enum>>(rhs));     \
+    }                                                            \
+    \
+    inline constexpr Enum operator^(Enum lhs, Enum rhs) noexcept \
+    {                                                            \
+        return static_cast<Enum>(                                \
+            static_cast<std::underlying_type_t<Enum>>(lhs) ^     \
+            static_cast<std::underlying_type_t<Enum>>(rhs));     \
+    }                                                            \
+    \
+    inline constexpr Enum operator~(Enum value) noexcept         \
+    {                                                            \
+        return static_cast<Enum>(~static_cast<std::underlying_type_t<Enum>>(value)); \
+    }                                                            \
+    \
+    inline constexpr Enum& operator|=(Enum& lhs, Enum rhs) noexcept \
+    {                                                               \
+        return lhs = lhs | rhs;                                     \
+    }                                                               \
+    \
+    inline constexpr Enum& operator&=(Enum& lhs, Enum rhs) noexcept \
+    {                                                               \
+        return lhs = lhs & rhs;                                     \
+    }                                                               
