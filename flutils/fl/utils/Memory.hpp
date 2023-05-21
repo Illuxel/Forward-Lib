fd09c4e62d0f2117b27754983840fecc8feca508
EEdle <83777873+EEdle506@users.noreply.github.com>
2023-05-22 02:03:05 +0300

    Create Memory.hpp

---
+++ flutils/fl/utils/Memory.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <memory>
+
+namespace fl {
+    
+    template<typename T>
+    using Scope = std::unique_ptr<T>;
+
+    template<typename T, typename ... Args>
+    constexpr Scope<T> MakeScope(Args&& ... args)
+    {
+        return std::make_unique<T>(std::forward<Args>(args)...);
+    }
+
+    template<typename T>
+    using Ref = std::shared_ptr<T>;
+
+    template<typename T, typename ... Args>
+    constexpr Ref<T> MakeRef(Args&& ... args)
+    {
+        return std::make_shared<T>(std::forward<Args>(args)...);
+    }
+
+} // namespace fl
