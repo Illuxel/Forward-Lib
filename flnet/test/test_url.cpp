3897ba714e94f324119727c2d8b42466cbf2cecf
EEdle <83777873+EEdle506@users.noreply.github.com>
2023-06-01 04:22:46 +0300

    Update test_url.cpp

--- flnet/test/test_url.cpp
+++ flnet/test/test_url.cpp
@@ -1,7 +1,13 @@
 #include "fl/net/http/HttpUrl.hpp"
+using namespace fl;
 
 #include <gtest/gtest.h>
 
-TEST(HttpUrl, IsTarget)
+TEST(HttpUrl, ParserTest)
 {
+    HttpUrl url("https://www.example.com/products/category/12345?page=2#section1");
+
+    EXPECT_EQ(url.Target(), "https://www.example.com/products/category/12345");
+    EXPECT_EQ(url.Section(), "section1");
+    EXPECT_EQ(url.Value("page"), "2");
 }
