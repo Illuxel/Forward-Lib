0e8626d22d6e2360572c6eb6d5e4aa71c097b3fe
EEdle <83777873+EEdle506@users.noreply.github.com>
2023-06-02 00:51:03 +0300

    Update response/request

--- flnet/fl/net/http/HttpRequest.hpp
+++ flnet/fl/net/http/HttpRequest.hpp
@@ -2,21 +2,42 @@
 
 #include "fl/net/Core.hpp"
 
+#include "fl/net/http/HttpUrl.hpp"
+
 namespace fl {
 
     template<typename Type>
     class HttpRequestWrapper
     {
-    private:
         http::request<Type> request_data_;
 
     public:
         HttpRequestWrapper(http::request<Type>&& request) 
             : request_data_(std::move(request)) {}
 
+        template<typename ...BodyArgs>
+        HttpRequestWrapper(std::piecewise_construct_t t, std::tuple<BodyArgs...> body_args) 
+            : request_data_{t, std::forward<BodyArgs>(body_args)...} {}
 
+        http::verb GetMethod() const{
+            return request_data_.method();
+        }
+        HttpUrl GetUrl() const {
+            return HttpUrl(request_data_.target());
+        }
 
+        int GetVersion() const {
+            return request_data_.version();
+        }
+
+        bool Alive() const {
+            return request_data_.keep_alive();
+        }
+        bool Alive(bool alive) const {
+            return request_data_.keep_alive(alive);
+        }
     };
 
-    using HttpRequest = HttpRequestWrapper<http::string_body>;
+    using HttpRequest = http::request<http::string_body>;
+    using HttpRequestEmpty = http::request<http::empty_body>;
 }
--- flnet/fl/net/http/HttpResponse.hpp
+++ flnet/fl/net/http/HttpResponse.hpp
@@ -7,16 +7,33 @@ namespace fl {
     template<typename Type>
     class HttpResponseWrapper
     {
-    private:
-        http::response<Type> request_data_;
+        http::response<Type> response_data_;
 
     public:
-        HttpResponseWrapper(http::response<Type>&& request)
-            : request_data_(std::move(request)) {}
+        HttpResponseWrapper(http::status status, int version)
+            : response_data_{status, version} {}
 
+        HttpResponseWrapper(http::response<Type>&& response)
+            : response_data_{std::move(response)} {}
+        HttpResponseWrapper(http::response<Type> const& response)
+            : response_data_{response} {}
 
+        template<typename ...BodyArgs>
+        HttpResponseWrapper(std::piecewise_construct_t t, std::tuple<BodyArgs...> body_args) 
+            : response_data_{t, std::forward<BodyArgs>(body_args)...} {}
 
+        void SetHeader(http::field field, std::string_view data) {
+            request_data_.set(field, data);
+        }
+
+        std::string& Body() & {
+            return request_data_.body();
+        }
+        std::string&& Body() && {
+            return std::move(request_data_.body());
+        }
     };
 
-    using HttpResponse = HttpResponseWrapper<http::string_body>;
+    using HttpResponse = http::response<http::string_body>;
+    using HttpResponseEmpty = http::response<http::empty_body>;
 }
