86c30e0b67ccd580d78352e4fa227dc9bd56a8cd
EEdle <83777873+EEdle506@users.noreply.github.com>
2023-06-01 04:23:01 +0300

    Update Url

--- flnet/fl/net/http/HttpUrl.cpp
+++ flnet/fl/net/http/HttpUrl.cpp
@@ -1,6 +1,71 @@
 #include "fl/net/http/HttpUrl.hpp"
 
+#include "fl/utils/StringArg.hpp"
+
+#include <algorithm>
+#include <iterator>
+
 namespace fl {
     
+    auto UrlParametersParser(std::string& url)
+    {
+        std::unordered_map<std::string, std::string> params;
+
+        while (true)
+        {
+            
+        }
+        
+        return params;
+    }
+
+    HttpUrl::HttpUrl()
+        : valid_(false) {}
+    HttpUrl::HttpUrl(std::string_view target)
+        : valid_(false) 
+    {
+        auto const pos = target.find('?');
+        if (pos == std::string::npos)
+            return;
+
+        target_ = target.substr(0, pos);
+        query_params_ = UrlParametersParser(target.substr(pos, target.size() - target_.size()));
+    }
+
+    std::string HttpUrl::Target() const
+    {
+        return target_;
+    }
+    std::string HttpUrl::Section() const
+    {
+        return section_;
+    }
+    std::string HttpUrl::Value(std::string_view key) const 
+    {
+        return "";
+    }
+
+    std::vector<std::string> HttpUrl::Keys() const
+    {
+        std::vector<std::string> keys;
+
+        std::transform(
+            query_params_.begin(), 
+            query_params_.end(), 
+            std::back_inserter(keys), 
+            [](auto const& pair){ return pair.first; }
+        );
+
+        return keys;
+    }
+    
+    bool HttpUrl::IsTarget() const 
+    {
+        return target_.empty();
+    }
+    bool HttpUrl::IsValid() const 
+    {
+        return valid_;
+    }
 
 } // namespace fl
--- flnet/fl/net/http/HttpUrl.hpp
+++ flnet/fl/net/http/HttpUrl.hpp
@@ -7,8 +7,8 @@ namespace fl {
     
     class HttpUrl
     {
-        bool valid_ = false;
-        std::string target_;
+        bool valid_;
+        std::string target_, section_;
         std::unordered_map<std::string, std::string> query_params_;
 
     public:
@@ -16,8 +16,12 @@ namespace fl {
         HttpUrl(std::string_view target);
     
         std::string Target() const;
+        std::string Section() const;
+
         std::string Value(std::string_view key) const;
 
+        std::vector<std::string> Keys() const;
+        
         bool IsTarget() const;
         bool IsValid() const;
     };
