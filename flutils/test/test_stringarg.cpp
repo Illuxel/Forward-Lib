8fcfb2127fc54c278f43e2c80ab97e324068250b
EEdle <83777873+EEdle506@users.noreply.github.com>
2023-06-06 23:14:57 +0300

    Add string arg test

---
+++ flutils/test/test_stringarg.cpp
@@ -0,0 +1,69 @@
+#include "fl/utils/StringArg.hpp"
+using namespace fl;
+
+#include <gtest/gtest.h>
+
+TEST(StringArg, ArgEqual) 
+{
+    using namespace fl;
+
+    StringArg arg1("test"), arg2;
+
+    EXPECT_FALSE(arg1 == arg2);
+
+    arg2.SetName("test");
+
+    EXPECT_TRUE(arg1 == arg2);
+    EXPECT_TRUE(arg1 == "Hidden test argument!");
+}
+TEST(StringArg, ArgCopy) 
+{
+    using namespace fl;
+
+    StringArg arg1("arg");
+
+    arg1.SetData("hello world");
+
+    StringArg copy(arg1);
+
+    EXPECT_TRUE(copy == arg1);
+    EXPECT_TRUE(arg1.Data() == copy.Data());
+
+    copy.SetName("NEW");
+
+    EXPECT_FALSE(copy == arg1);
+}
+
+TEST(StringArg, MakeArg) 
+{
+    using namespace fl;
+
+    std::string_view str = "arg_name=arg_data";
+    auto arg = StringArg::FromString(str, '=');
+
+    EXPECT_TRUE(arg == "arg_name");
+    EXPECT_TRUE(arg.Data() == "arg_data");
+}
+
+TEST(StringArgSpecifier, ArgEqual) 
+{
+    using namespace fl;
+
+    StringArg arg1("test", '%'), arg2('%');
+
+    EXPECT_FALSE(arg1 == arg2);
+
+    arg2.SetName("test");
+
+    EXPECT_TRUE(arg1 == arg2);
+    EXPECT_TRUE(arg1 == "Hidden %test argument!");
+}
+TEST(StringArgSpecifier, ArgCopy) 
+{
+    using namespace fl;
+
+    StringArg arg1("arg", '%');
+    StringArg copy(arg1);
+
+    EXPECT_TRUE(copy == arg1);
+}
