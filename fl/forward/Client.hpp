8a7858a80ebc609590bebcab473716a5aff8190f
EEdle <83777873+EEdle506@users.noreply.github.com>
2023-06-22 06:37:56 +0300

    Initial Client

--- fl/forward/Client.hpp
+++ fl/forward/Client.hpp
@@ -1,11 +1,32 @@
 #pragma once
 
+#include "fl/utils/Log.hpp"
+
+#include "fl/net/http/Core.hpp"
+
 namespace fl {
 
-    class Client 
+    class Client : public std::enable_shared_from_this<Client>
     {
-    public:
+        tcp::resolver resolver_;
+        beast::ssl_stream<beast::tcp_stream> stream_;
+        beast::flat_buffer buffer_; // (Must persist between reads)
+        http::request<http::empty_body> req_;
+        http::response<http::string_body> res_;
 
-    private:
+    public:
+        explicit Client(net::any_io_executor ex, ssl::context& ctx);
+
+        // Start the asynchronous operation
+        void Run( char const* host, char const* port,char const* target, int version);
+
+        void OnResolve(beast::error_code ec, tcp::resolver::results_type results);
+        void OnConnect(beast::error_code ec, tcp::resolver::results_type::endpoint_type);
+        void OnHandshake(beast::error_code ec);
+
+        void OnWrite(beast::error_code ec, std::size_t bytes_transferred);
+        void OnRead(beast::error_code ec, std::size_t bytes_transferred);
+        
+        void OnShutdown(beast::error_code ec);
     };
 }
---
+++ test/test_client.cpp
@@ -0,0 +1,31 @@
+#include "fl/forward/Client.hpp"
+using namespace fl;
+
+#include <gtest/gtest.h>
+
+TEST(Client, Connection)
+{
+    // The io_context is required for all I/O
+    net::io_context ioc;
+
+    // The SSL context is required, and holds certificates
+    ssl::context ctx{ssl::context::tlsv12_client};
+
+    // This holds the root certificate used for verification
+    // load_root_certificates(ctx);
+
+    // Verify the remote server's certificate
+    ctx.set_verify_mode(ssl::verify_peer);
+
+    // Launch the asynchronous operation
+    // The session is constructed with a strand to
+    // ensure that handlers do not execute concurrently.
+    MakeRef<Client>(
+        net::make_strand(ioc),
+        ctx
+        )->Run("api.forchat.online", "8080", "/login", 11);
+
+    // Run the I/O service. The call will return when
+    // the get operation is complete.
+    ioc.run();
+}
