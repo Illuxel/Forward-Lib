b2129d19e18d99534354d3b34d99489fd6870ce2
EEdle <83777873+EEdle506@users.noreply.github.com>
2023-07-15 21:45:59 +0300

    Update naming

--- flutils/fl/utils/Memory.hpp
+++ flutils/fl/utils/Memory.hpp
@@ -4,22 +4,22 @@
 
 namespace Forward {
     
-    template<typename T>
-    using Scope = std::unique_ptr<T>;
+    template<class Type>
+    using Scope = std::unique_ptr<Type>;
 
-    template<typename T, typename ... Args>
-    constexpr Scope<T> MakeScope(Args&& ... args)
+    template<typename Type, typename ...Args>
+    constexpr Scope<Type> MakeScope(Args&& ...args)
     {
-        return std::make_unique<T>(std::forward<Args>(args)...);
+        return std::make_unique<Type>(std::forward<Args>(args)...);
     }
 
-    template<typename T>
-    using Ref = std::shared_ptr<T>;
+    template<class Type>
+    using Ref = std::shared_ptr<Type>;
 
-    template<typename T, typename ... Args>
-    constexpr Ref<T> MakeRef(Args&& ... args)
+    template<class Type, typename ...Args>
+    constexpr Ref<Type> MakeRef(Args&& ...args)
     {
-        return std::make_shared<T>(std::forward<Args>(args)...);
+        return std::make_shared<Type>(std::forward<Args>(args)...);
     }
 
 } // namespace Forward
