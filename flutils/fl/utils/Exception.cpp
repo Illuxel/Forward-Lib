eb7845ded12072d181ffc58259c076e3501d2bde
EEdle <83777873+EEdle506@users.noreply.github.com>
2023-07-17 02:49:53 +0300

    Add thread safety

--- flutils/fl/utils/Exception.cpp
+++ flutils/fl/utils/Exception.cpp
@@ -1,36 +1,65 @@
 #include "fl/utils/Exception.hpp"
-#include "Exception.hpp"
 
 namespace Forward {
 
     Exception::Exception()
         : std::exception{}
-        , is_error(false) {}
+        , is_error(false) 
+    {
+    }
     Exception::Exception(std::string_view msg)
         : std::exception{}
         , is_error(true)
     {
-        msg_.emplace(msg);
+        msg_ = msg;
     }
     Exception::Exception(std::exception const& ec)
         : std::exception{}
-        , is_error(true) 
     {
-        msg_.emplace(ec.what());
+        SetError(ec);
+    }
+    Exception::Exception(Exception const& ec)
+        : std::exception(ec)
+        , is_error(ec.is_error)
+    {
+        msg_ = ec.msg_;
     }
 
     Exception::~Exception() {}
 
-    Exception& Exception::operator=(std::string_view msg)
+    void Exception::SetError(std::exception const& ec)
     {
-        is_error = true; 
-        msg_.emplace(msg);
+        std::unique_lock lock(mtx_);
+
+        msg_ = ec.what();
+        is_error = true;
+    }
+
+    char const* Exception::what() const noexcept
+    {
+        std::shared_lock lock(mtx_);
+        if (msg_.empty())
+            return msg_.c_str();
+        return "Unknown exception";
+    }
+
+    bool Exception::IsError() const
+    {
+        std::shared_lock lock(mtx_);
+        return is_error;
+    }
+
+    Exception& Exception::operator=(std::string_view msg) noexcept
+    {
+        std::unique_lock lock(mtx_);
+
+        is_error = true;        
+        msg_ = msg;
         return *this;
     }
-    Exception& Exception::operator=(std::exception const &ec)
+    Exception& Exception::operator=(std::exception const& ec) noexcept 
     {
-        is_error = true;
-        msg_.emplace(ec.what());
+        SetError(ec);
         return *this;
     }
 
--- flutils/fl/utils/Exception.hpp
+++ flutils/fl/utils/Exception.hpp
@@ -5,43 +5,57 @@
 
 #include <exception>
 
+#include <shared_mutex>
+
 namespace Forward {
     
+    /**
+    *   Thread safe exception class 
+    */
+
     class Exception : public std::exception
     {
     private:
         bool is_error;
-        std::optional<std::string> msg_;
+        std::string msg_;
+        mutable std::shared_mutex mtx_;
 
     public:
         Exception();
-        
+        explicit Exception(Exception const& ec);
         explicit Exception(std::string_view msg);
         explicit Exception(std::exception const& ec);
 
         virtual ~Exception() override;
 
-        virtual char const* what() const noexcept override
-        {
-            if (msg_.has_value())
-                return msg_.value().c_str();
-            return "Unknown exception";
-        }
+        void SetError(std::exception const& ec);
 
-        operator bool() const 
+        bool IsError() const;
+
+        virtual char const* what() const noexcept override;
+
+        Exception& operator=(std::string_view msg) noexcept;
+        Exception& operator=(std::exception const& ec) noexcept;
+
+        operator bool() const noexcept
         {
-            return is_error;
+            return IsError();
         }
-        operator const char*() const 
+        operator const char*() const noexcept
         {
             return what();
         }
-        operator std::string() const 
+        operator std::string() const noexcept
         {
-            return msg_.value_or("Unknown exception");
+            return what();
         }
 
-        Exception& operator=(std::string_view msg);
-        Exception& operator=(std::exception const& ec);
+        template<class Ch, class Tr>
+        friend std::basic_ostream<Ch, Tr>&
+        operator<<(std::basic_ostream<Ch, Tr>& os, Exception const& ec)
+        {
+            std::shared_lock lock(mtx_);
+            return os << ec.what();
+        }
     };
 } // namespace Forward
