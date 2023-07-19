8451a2b8e0adfb1f5f7d7a8571d50a667dabac4c
EEdle <83777873+EEdle506@users.noreply.github.com>
2023-07-20 02:16:29 +0300

    Update examples folder

---
+++ examples/client/UseClientAPI.cpp
@@ -0,0 +1,7 @@
+#include "fl/forward/Client.hpp"
+using namespace Forward;
+
+int main(int argc, char* argv[])
+{
+	return EXIT_SUCCESS;
+}
--- examples/db/UseDBConnect.cpp
+++ examples/db/UseDBConnect.cpp
@@ -1,19 +1,23 @@
 /**
  *
- * Base SINGLE thread example of using MySQL Connector Wrapper   
+ * Base SINGLE thread example of using Forward Wrapper   
  *
  */
 
 #include "fl/db/Database.hpp"
 using namespace Forward;
 
+// Change this data for your database
+
 #define ADDRESS	"tcp://127.0.0.1:3306"		
 #define USER_NAME "root"
 #define USER_PASSWORD "testing"
 
-#define DB_NAME "test"
+#define DB_NAME "test" 
 
-int main(int, char*)
+//
+
+int main(int argc, char* argv[])
 {
 	// Making scope object to destroy it after method exit
 	auto db = Database::InitScoped();
@@ -47,25 +51,18 @@ int main(int, char*)
 		// Make sure to provide correct variable name like 'current_time' or using index of a row
 		// Also you can use prepared queries using '?' as value that will be inserted
 
-		auto result = db->Execute("SELECT NOW()");
-
-		// Checking if result is valid
-
-		if (!result)
-		{
-			return EXIT_FAILURE;
-		}
+		DBTypes::Result result = db->Execute("SELECT NOW()");
 
 		// Checking if result has first row
 
-		if (!result->first())
+		if (!result.IsEmpty())
 		{
 			return EXIT_FAILURE;
 		}
 
 		// Getting column from row with index '1'
 
-		auto time = result->getString(1);
+		auto time = result.Get<std::string>(1);
 
 		std::cout << "Current DB time is " << time;
 	}
