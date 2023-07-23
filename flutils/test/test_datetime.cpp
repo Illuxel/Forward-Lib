7626a55c2a88bbf5a66cf035f0523b1318a3c624
EEdle <83777873+EEdle506@users.noreply.github.com>
2023-07-23 15:54:44 +0300

    Update tests

--- flutils/test/test_datetime.cpp
+++ flutils/test/test_datetime.cpp
@@ -7,15 +7,18 @@ TEST(DateTime, ToCFormat)
 {
     std::string out = DateTime::ConvertToCFormat("DD-MM-YYYY hh-mm-ss");
 
-    EXPECT_STREQ(out.c_str(), "%d-%m-%Y %H-%M-%S");
+    EXPECT_TRUE(out == "%d-%m-%Y %H-%M-%S");
 }
 
 TEST(DateTime, NowToString)
 {
     std::string now = DateTime::Now().ToString("DD-MM-YYYY hh-mm-ss");
-    std::string latenow = DateTime::Now().ToString("DD-MM-YYYY hh-mm-ss");
 
-    EXPECT_STREQ(now.c_str(), latenow.c_str());
+    std::this_thread::sleep_for(std::chrono::milliseconds(500));
+
+    std::string late_now = DateTime::Now().ToString("DD-MM-YYYY hh-mm-ss");
+
+    EXPECT_FALSE(now == late_now);
 }
 
 TEST(DateTime, CopyEq)
@@ -26,10 +29,11 @@ TEST(DateTime, CopyEq)
     EXPECT_TRUE(now == copy);
 }
 
-#include <optional>
-
-TEST(DateTime, Exception)
+TEST(DateTime, FailParse)
 {
     std::string now = DateTime::Now().ToString("dafsd   4252  2s");
-    std::string latenow = DateTime::Now().ToString("strange text");
+    std::string late_now = DateTime::Now().ToString("strange text");
+
+    EXPECT_TRUE(now.empty());
+    EXPECT_TRUE(late_now.empty());
 }
