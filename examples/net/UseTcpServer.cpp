0eb9d0d4e09c5ca0419111618366befe3fdf0c64
EEdle <83777873+EEdle506@users.noreply.github.com>
2023-07-29 23:07:48 +0300

    Init TcpServer example

---
+++ examples/net/UseTcpServer.cpp
@@ -0,0 +1,11 @@
+#include "fl/net/TcpServer.hpp"
+using namespace Forward;
+
+int main(int argc, char* argv[])
+{
+	TcpServer server;	// Initialize server in single thread, using localhost and port 80
+
+
+
+	return EXIT_SUCCESS;
+}
