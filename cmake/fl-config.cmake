3c60a9b93609d9eb0a6937b5a4f52c57094fdd8a
EEdle <83777873+EEdle506@users.noreply.github.com>
2023-07-30 18:25:37 +0300

    Clean install scripts

--- cmake/fl-config.cmake
+++ cmake/fl-config.cmake
@@ -1,6 +1,2 @@
-@PACKAGE_INIT@
-
 include(CMakeFindDependencyMacro)
-include("${CMAKE_CURRENT_LIST_DIR}/fl-targets.cmake")
-
-check_required_components(@PROJECT_NAME@)
+include(${CMAKE_CURRENT_LIST_DIR}/fl-targets.cmake)
