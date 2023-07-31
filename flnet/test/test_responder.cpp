0c98757074e01bb74f3dc8fef20caffff4e1a5f2
EEdle <83777873+EEdle506@users.noreply.github.com>
2023-07-31 19:53:43 +0300

    Update test. Change method name

--- flnet/test/test_responder.cpp
+++ flnet/test/test_responder.cpp
@@ -20,12 +20,12 @@ TEST(HttpResponder, RegistrationTest) {
     responder.SetBadHandler(
     [](HttpRequest const& req, http::status status) 
     {
-        HttpResponse res(status, req.Base().version());
-        res.Base().set(http::field::server, BOOST_BEAST_VERSION_STRING);
-        res.Base().set(http::field::content_type, MimeType::FromString("html").GetMimeName());
-        res.Base().keep_alive(req.Base().keep_alive());
-        res.Base().body() = "Can not find page or resource";
-        res.Base().prepare_payload();
+        HttpResponse res(status, req.base().version());
+        res.base().set(http::field::server, BOOST_BEAST_VERSION_STRING);
+        res.base().set(http::field::content_type, MimeType::FromString("html").GetMimeName());
+        res.base().keep_alive(req.base().keep_alive());
+        res.base().body() = "Can not find page or resource";
+        res.base().prepare_payload();
 
         return res;
     });
