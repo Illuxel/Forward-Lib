b12831e038bf3efbc8cb2cc14cc7ec2222b98363
EEdle <83777873+EEdle506@users.noreply.github.com>
2023-08-10 20:58:09 +0300

    Update install script

--- cmake/fl-config.cmake
+++ cmake/fl-config.cmake
@@ -1,2 +1,9 @@
-include(CMakeFindDependencyMacro)
+@PACKAGE_INIT@
+
 include(${CMAKE_CURRENT_LIST_DIR}/fl-targets.cmake)
+include(CMakeFindDependencyMacro)
+
+find_dependency(OpenSSL)
+find_dependency(Boost)
+# find_dependency(jwt-cpp)
+# find_dependency(SQLite)
