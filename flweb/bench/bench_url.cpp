28047b1d49c91e3ca9f98cfd8ac55f6b0c4416dc
EEdle <83777873+EEdle506@users.noreply.github.com>
2023-10-20 03:54:13 +0300

    Add benchmark

---
+++ flweb/bench/bench_url.cpp
@@ -0,0 +1,15 @@
+#include "fl/web/HttpUrl.hpp"
+using namespace Forward::Web;
+
+#include <benchmark/benchmark.h>
+
+static void BM_UrlParse(benchmark::State& state) 
+{
+    for (auto _ : state) 
+    {
+        HttpUrl url("http://www.example.com/path/to/resource?param1=value1#fragment");
+    }
+}
+BENCHMARK(BM_UrlParse);
+
+BENCHMARK_MAIN();
